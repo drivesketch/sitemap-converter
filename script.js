46097d8543fcaed4cfbed114a81c0ac80a030ae0
so <hiroaki.so@smarthr.co.jp>
2023-04-20 15:27:33 +0900

    インデントがつくように変更

--- script.js
+++ script.js
@@ -12,14 +12,49 @@ button_convert.addEventListener('click', () => {
     const array_byLine = text_input.split('\n'); // 1行ずつ配列に格納する
     array_byLine.shift(); // ヘッダ行の分を削除
     array_byLine.sort();  // ソートする（＝URL順になる）
-    let text_output = `URL\tTitle\tRoot\tL2\tL3\tL4\tL5\tL6`; //新ヘッダ行
-    for (let i = 0; i < array_byLine.length; i++ ){
-        array_byLine[i] = array_byLine[i].replace(/^\"(.*)\"$/g,'$1'); //最初と最後のクォートを消す
-        const array_byComma = array_byLine[i].split('\",\"') // "," の塊ごとに区切って配列化し、[URL, Title, xxx, xxx, ...] を作る
-        array_byComma.splice(2, array_byComma.length - 2); //URLとTitle以外を削除し、[URL, Title] だけにする
-        array_byComma.push(array_byComma[0]); // URLを複製して末尾に追加
-        array_byComma[2] = array_byComma[2].replace(/([^\/])\/([^(\/|\n\$)])/g,'$1\/\t$2');  //末尾の方のURLに、スラッシュごとにtab文字を挿入
-        text_output += `\n${array_byComma[0]}\t${array_byComma[1]}\t${array_byComma[2]}\n` //タブ区切りテキスト化する
+
+    const num_pages = array_byLine.length;
+    const array_title = [];
+    const array_url = [];
+    const array_depth = [];
+    let depth_max = 0;
+
+    // 1行ずつ処理する
+    // title, url, depthそれぞれの行ごとの配列を作る
+    for (let i = 0; i < num_pages; i++ ){
+        // カンマごとに配列に格納する
+        // ダブルクオーテーションを取る
+        const array_byComma = array_byLine[i].replace(/^\"(.*)\"$/g,'$1').split('","'); 
+        const [url, title] = array_byComma;
+        const url_tabbed = url.replace(/([^\/])\/([^(\/|\n\$)])/g,'$1\/\t$2');
+        const depth = url_tabbed.split('\t').length;
+
+        // 階層の深さが最大を超える場合、最大を上書きする
+        depth > depth_max ? depth_max = depth : null;
+
+        // 配列に追加する
+        array_title.push(title);
+        array_url.push(url + '\t' + url_tabbed);
+        array_depth.push(depth);
+    }
+
+    // ヘッダ行を作る
+    // 最大深度に応じて Root, L2, L3,... の数字が増える
+    let header_depth = '';
+    for (let i = 0; i < depth_max; i++){
+        header_depth += (i === 0) ? 'Root' : `\tL${i+1}`;
+    }
+    let text_output = `${header_depth}\tURL\t${header_depth}`;
+
+    // 出力文を作る
+    for (let i = 0; i < num_pages; i++ ){
+        // 階層の深さに合わせてTitleのインデントを調整する
+        const num_tabs_before = array_depth[i] - 1;
+        const tabs_before = '\t'.repeat(num_tabs_before);
+        const num_tabs_after = depth_max - array_depth[i] + 1;
+        const tabs_after = '\t'.repeat(num_tabs_after);
+
+        text_output += `\n${tabs_before}${array_title[i]}${tabs_after}${array_url[i]}`;
     }
     area_output.value = text_output;
 });
